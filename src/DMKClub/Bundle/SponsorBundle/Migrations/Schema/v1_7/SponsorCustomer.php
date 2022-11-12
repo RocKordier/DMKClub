b46191960b5959f8f83fc4f2a42ea29e54120d69
René Nitzsche <rene@system25.de>
2022-11-12 15:40:34 +0100

    Add customer migration

---
+++ src/DMKClub/Bundle/SponsorBundle/Migrations/Schema/v1_7/SponsorCustomer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DMKClub\Bundle\SponsorBundle\Migrations\Schema\v1_7;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\SalesBundle\Migration\Extension\CustomerExtensionAwareInterface;
+use Oro\Bundle\SalesBundle\Migration\Extension\CustomerExtensionTrait;
+
+class SponsorCustomer implements Migration, CustomerExtensionAwareInterface
+{
+    use CustomerExtensionTrait;
+
+	/**
+	 * @inheritdoc
+	 * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+	 */
+	public function up(Schema $schema, QueryBag $queries)
+	{
+	    $this->customerExtension->addCustomerAssociation($schema, 'dmkclub_sponsor');
+	}
+}
