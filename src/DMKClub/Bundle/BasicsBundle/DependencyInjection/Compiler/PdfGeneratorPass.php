a55e5180be8279de3611098b8993c960ae666eff
RocKordier <eric@ehdev.de>
2024-04-01 16:48:54 +0200

    [Upgrade6] BasicsBundle/DependencyInjection

--- src/DMKClub/Bundle/BasicsBundle/DependencyInjection/Compiler/PdfGeneratorPass.php
+++ src/DMKClub/Bundle/BasicsBundle/DependencyInjection/Compiler/PdfGeneratorPass.php
@@ -1,43 +1,35 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+declare(strict_types=1);
 
 namespace DMKClub\Bundle\BasicsBundle\DependencyInjection\Compiler;
 
-use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use DMKClub\Bundle\BasicsBundle\PDF\Manager;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Adds tagged dmkclub_basics.pdfgenerator services to pdf service.
  */
 class PdfGeneratorPass implements CompilerPassInterface
 {
-	const TAG = 'dmkclub_basics.pdfgenerator';
-	const MANAGER = Manager::class;
+    public const string TAG = 'dmkclub_basics.pdfgenerator';
+    public const string MANAGER = Manager::class;
 
-	public function process(ContainerBuilder $container)
-	{
-		if (false === $container->hasDefinition(self::MANAGER)) {
-			return;
-		}
+    public function process(ContainerBuilder $container): void
+    {
+        if (false === $container->hasDefinition(self::MANAGER)) {
+            return;
+        }
 
-		$definition = $container->getDefinition(self::MANAGER);
-		$taggedServices = $container->findTaggedServiceIds(self::TAG);
-		foreach (array_keys($taggedServices) as $id) {
-			$definition->addMethodCall(
-					'addGenerator',
-					array(new Reference($id))
-			);
-		}
-
-	}
+        $definition = $container->getDefinition(self::MANAGER);
+        $taggedServices = $container->findTaggedServiceIds(self::TAG);
+        foreach (array_keys($taggedServices) as $id) {
+            $definition->addMethodCall(
+                'addGenerator',
+                [new Reference($id)]
+            );
+        }
+    }
 }
--- src/DMKClub/Bundle/BasicsBundle/DependencyInjection/DMKClubBasicsExtension.php
+++ src/DMKClub/Bundle/BasicsBundle/DependencyInjection/DMKClubBasicsExtension.php
@@ -1,23 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace DMKClub\Bundle\BasicsBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
- * This is the class that loads and manages your bundle configuration
+ * This is the class that loads and manages your bundle configuration.
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
 class DMKClubBasicsExtension extends Extension
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $this->processConfiguration($configuration, $configs);
