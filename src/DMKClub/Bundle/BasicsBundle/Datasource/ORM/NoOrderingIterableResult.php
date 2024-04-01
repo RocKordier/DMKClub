f23d3b0630e6794eac4ac25f02384b8740ef1386
RocKordier <eric@ehdev.de>
2024-04-01 16:45:52 +0200

    [Upgrade6] BasicsBundle/Datasource

--- src/DMKClub/Bundle/BasicsBundle/Datasource/ORM/NoOrderingIterableResult.php
+++ src/DMKClub/Bundle/BasicsBundle/Datasource/ORM/NoOrderingIterableResult.php
@@ -1,20 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace DMKClub\Bundle\BasicsBundle\Datasource\ORM;
 
 use Oro\Bundle\BatchBundle\ORM\Query\ResultIterator\IdentifierWithoutOrderByIterationStrategy;
+use Oro\Bundle\BatchBundle\ORM\Query\ResultIterator\IdentityIterationStrategyInterface;
 use Oro\Bundle\DataGridBundle\Datasource\Orm\IterableResult;
 
 /**
  * Ermöglicht die Iteration über komplexe Datagrids. Dabei wird aber auf die Sortierung verzichtet.
- *
  */
 class NoOrderingIterableResult extends IterableResult
 {
-    /**
-     * {@inheritdoc}
-     */
-    protected function getIterationStrategy()
+    protected function getIterationStrategy(): IdentityIterationStrategyInterface
     {
         if (null === $this->iterationStrategy) {
             $this->iterationStrategy = new IdentifierWithoutOrderByIterationStrategy();
